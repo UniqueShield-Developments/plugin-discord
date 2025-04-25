7fe47d82cf6e6531ebec71e6ecb0428667393916
Hermet1c <admin@uniqueshield.dev>
2025-04-25 21:14:10 +0200

    2 Changes Changed some callback parameters Added and exported EmedBuilder classes

--- index.d.ts
+++ index.d.ts
@@ -2,23 +2,29 @@ import {
     Message,
     TextBasedChannel,
     MessagePayload,
-    MessageCreateOptions
+    MessageCreateOptions,
 } from 'discord.js'
 
-type BasicMessageData = {
+
+type DiscordMessageBase = {
     id: string
     content: string
+    channelId: string
+    guildId?: string
+    editedTimestamp?: string | null
+}
+
+type DiscordBasicMessageData = DiscordMessageBase & {
     author: {
         id: string
         username: string
         discriminator: string
         avatar?: string | null
-    }
-    channelId: string
-    guildId?: string
+    },
     timestamp: string
-    editedTimestamp?: string | null
+
 }
+
 
 
 type GenericLogEditCallback = (data: any) => void
@@ -30,7 +36,7 @@ type JoinCallback = (data: PlayerBase) => void
 type LeaveCallback = (data: LeaveCallbackParams) => void
 type KicksCallback = (data: KicksCallbackParams) => void
 
-type MessageCallback = (message: BasicMessageData) => void
+type DiscordMessageCallback = (message: DiscordBasicMessageData) => void
 
 interface LogCategory<T extends (...args: any) => void = GenericLogEditCallback> {
     edit(cb: T): void
@@ -44,21 +50,21 @@ interface PlayerBase {
     username: string
     xuid: string
     pfpLink: string
+    device: string
 }
 
 interface PlayerChatCallbackParams extends PlayerBase, MessageBase { }
 
 interface LeaveCallbackParams extends PlayerBase {
-    timePlayed: number
+    timePlayed: string
 }
 
 interface KicksCallbackParams extends PlayerBase {
-    timePlayed: number
     reason: string
     extraReason: {
         reasonType: string
         reason: string
-    }
+    } | null
 }
 
 interface Logs {
@@ -74,25 +80,101 @@ interface Logs {
 /**
  * 
  * @param cb The function to be executed when a message is recieved
- */
-declare function onMessage(cb: MessageCallback): void
+*/
+declare function onMessage(cb: DiscordMessageCallback): void
+
 /**
  * Sends a message to a specified channel
  * 
  * @param message The message to send to the channel. Can be a string, MessagePayload, or MessageCreateOptions
  * @param channel The channel where the message will be sent
- */
+*/
 declare function sendMessage(
     message: string | MessagePayload | MessageCreateOptions,
     channel: TextBasedChannel
-): void
+): Promise<DiscordMessageBase>
 
 declare const logs: Logs
 /**
  * The channel id where the join command was executed in
- */
+*/
 declare const channelId: string
 /**
  * The id of the discord server that this bot is linked to
- */
+*/
 declare const guildId: string
+
+/**
+ * A class to build and manage Discord embeds
+ */
+export declare class EmbedBuilder {
+    /**
+     * Sets the title of the embed
+     * @param title The title of the embed
+     */
+    setTitle(title: string): this;
+
+    /**
+     * Sets the description of the embed
+     * @param description The description of the embed
+     */
+    setDescription(description: string): this;
+
+    /**
+     * Sets the color of the embed
+     * @param color The color of the embed in hexadecimal or integer format
+     */
+    setColor(color: string | number): this;
+
+    /**
+     * Adds a field to the embed
+     * @param name The name of the field
+     * @param value The value of the field
+     * @param inline Whether the field should be displayed inline
+     */
+    addField(name: string, value: string, inline?: boolean): this;
+
+    /**
+     * Sets the footer of the embed
+     * @param text The footer text
+     * @param iconURL The URL of the footer icon
+     */
+    setFooter(text: string, iconURL?: string): this;
+
+    /**
+     * Sets the timestamp of the embed
+     * @param timestamp The timestamp to set, defaults to the current time
+     */
+    setTimestamp(timestamp?: Date): this;
+
+    /**
+     * Sets the URL of the embed
+     * @param url The URL to set
+     */
+    setURL(url: string): this;
+
+    /**
+     * Sets the author of the embed
+     * @param name The name of the author
+     * @param iconURL The URL of the author's icon
+     * @param url The URL of the author
+     */
+    setAuthor(name: string, iconURL?: string, url?: string): this;
+
+    /**
+     * Sets the thumbnail of the embed
+     * @param url The URL of the thumbnail image
+     */
+    setThumbnail(url: string): this;
+
+    /**
+     * Sets the image of the embed
+     * @param url The URL of the image
+     */
+    setImage(url: string): this;
+
+    /**
+     * Converts the embed to a JSON object
+     */
+    toJSON(): object;
+}
